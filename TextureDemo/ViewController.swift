1d5ef6c6895dfef7acb998a00bf231316a074cff
aleksey_pak_aa <aleksey.pakaa@gmail.com>
2018-05-18 00:47:03 +0300

    --

--- TextureDemo/ViewController.swift
+++ TextureDemo/ViewController.swift
@@ -9,28 +9,12 @@
 import UIKit
 import AsyncDisplayKit
 
-class ViewController: ASViewController<ViewController.ContentNode> {
+class ViewController: ASViewController<ContentNode> {
 
 	let contentNode: ContentNode
-	class ContentNode: ASDisplayNode {
 
-		let tableNode: ASTableNode = ASTableNode(style: .plain)
-
-		override init() {
-			super.init()
-
-			automaticallyManagesSubnodes = true
-		}
-
-		override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-			return ASWrapperLayoutSpec(layoutElement: tableNode)
-		}
-	}
 
 	var items: [String] = []
-
-	//using to continue scrolling ater adding new elements
-	var targetContentOffset: CGPoint = .zero
 
 	init() {
 		contentNode = ContentNode()
@@ -41,25 +25,63 @@ class ViewController: ASViewController<ViewController.ContentNode> {
 
 		contentNode.tableNode.dataSource = self
 		contentNode.tableNode.delegate = self
+        
+        contentNode.tableNode.view.contentInsetAdjustmentBehavior = .never
 	}
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 	override func viewDidLoad() {
 		let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTouchAddButton))
 		navigationItem.rightBarButtonItem = addButton
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
 	}
+    
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        guard let frame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        guard let duartion = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? Double else {
+            return
+        }
+        
+        contentNode.keyboardFrame = frame
+        UIView.animate(withDuration: duartion, animations: {
+            self.contentNode.transitionLayout(withAnimation: true, shouldMeasureAsync: true)
+        })
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        guard let frame = notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? CGRect else {
+            return
+        }
+        
+        guard let duartion = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? Double else {
+            return
+        }
+        
+        contentNode.keyboardFrame = .zero
+        UIView.animate(withDuration: duartion, animations: {
+            self.contentNode.transitionLayout(withAnimation: true, shouldMeasureAsync: true)
+        })
+
+    }
 
 	override func viewDidLayoutSubviews() {
-		super.viewDidLayoutSubviews()
-
-		contentNode.tableNode.view.contentInsetAdjustmentBehavior = .never
-
-		let inset = (navigationController?.navigationBar.frame.height ?? 0.0) + UIApplication.shared.statusBarFrame.height
-		contentNode.tableNode.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: inset, right: 0.0)
-		contentNode.tableNode.view.scrollIndicatorInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: inset, right: 0.0)
-	}
-
-	required init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+        super.viewDidLayoutSubviews()
+        
+        let bottomInset = (navigationController?.navigationBar.frame.height ?? 0.0) + UIApplication.shared.statusBarFrame.height
+        let topInset = contentNode.chatInputControlNode.bounds.height
+        
+        contentNode.tableNode.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: bottomInset, right: 0.0)
+        contentNode.tableNode.view.scrollIndicatorInsets = UIEdgeInsets(top: topInset, left: 0.0, bottom: bottomInset, right: 0.0)
 	}
 
 	@objc func didTouchAddButton() {
@@ -72,14 +94,13 @@ class ViewController: ASViewController<ViewController.ContentNode> {
 			contentNode.tableNode.performBatchUpdates({
 				self.contentNode.tableNode.insertRows(at: [indexPath], with: .top)
 			}, completion: {_ in
-				let contentOffset: CGPoint = {
-					let x: CGFloat = self.contentNode.tableNode.contentOffset.x
-					let y: CGFloat = 0.0
-
-					return CGPoint(x: x, y: y)
-				}()
-
-				self.contentNode.tableNode.setContentOffset(contentOffset, animated: true)
+                let offset: CGPoint = {
+                    let x = self.contentNode.tableNode.contentOffset.x
+                    let y = -self.contentNode.tableNode.contentInset.top
+                    
+                    return CGPoint(x: x, y: y)
+                }()
+				self.contentNode.tableNode.setContentOffset(offset, animated: true)
 			})
 		} else {
 			UIView.performWithoutAnimation {
@@ -89,7 +110,7 @@ class ViewController: ASViewController<ViewController.ContentNode> {
 
 				guard !contentNode.tableNode.view.isDecelerating else { return }
 
-				let contentOffset: CGPoint = {
+				let offset: CGPoint = {
 					let heightDiff = newContentSize.height - oldContentSize.height
 
 					let x = contentNode.tableNode.contentOffset.x
@@ -98,7 +119,7 @@ class ViewController: ASViewController<ViewController.ContentNode> {
 					return CGPoint(x: x, y: y)
 				}()
 
-				contentNode.tableNode.contentOffset = contentOffset
+				contentNode.tableNode.contentOffset = offset
 			}
 		}
 	}
@@ -108,13 +129,6 @@ class ViewController: ASViewController<ViewController.ContentNode> {
 
 extension ViewController: ASTableDelegate {
 
-	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-		self.targetContentOffset = targetContentOffset.pointee
-	}
-
-	func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-		//self.targetContentOffset = .zero
-	}
 }
 
 extension ViewController: ASTableDataSource {
@@ -134,27 +148,80 @@ extension ViewController: ASTableDataSource {
 
 }
 
+class ContentNode: ASDisplayNode {
+    
+    var keyboardFrame: CGRect = .zero
+    
+    let tableNode: ASTableNode = ASTableNode(style: .plain)
+    let chatInputControlNode:ASEditableTextNode = {
+        let node = ASEditableTextNode()
+        
+        node.backgroundColor = .red
+        node.scrollEnabled = false
+        return node
+    } ()
+    
+    override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let relativeLayout = ASRelativeLayoutSpec(horizontalPosition: .none,
+                                                  verticalPosition: .end,
+                                                  sizingOption: [],
+                                                  child: chatInputControlNode)
+        
+        let insets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardFrame.height, right: 0.0)
+        let insetLayout = ASInsetLayoutSpec(insets: insets, child: relativeLayout)
+        
+        return ASBackgroundLayoutSpec(child: insetLayout, background: tableNode)
+    }
+}
 
+class ChatInputControlNode: ASDisplayNode {
+    
+    let ediatbleTextNode: ASEditableTextNode = {
+        let node = ASEditableTextNode()
+        
+        node.backgroundColor = .white
+        node.maximumLinesToDisplay = 5
+        node.scrollEnabled = false
+        return node
+    } ()
+    let sendButtonNode: ASButtonNode = ASButtonNode()
+    
+    override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let insets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        return ASInsetLayoutSpec(insets: insets, child: ediatbleTextNode)
+    }
+    
+}
 
 class CellNode: ASCellNode {
 
-	var titleTextNode: ASTextNode
-	var descriptionTextNode: ASTextNode
+	var titleTextNode: ASTextNode  = ASTextNode()
+	var descriptionTextNode: ASTextNode  = ASTextNode()
 
 	override init() {
-		titleTextNode = ASTextNode()
-		descriptionTextNode = ASTextNode()
-
 		super.init()
 
+        automaticallyManagesSubnodes = true
+        selectionStyle = .none
+        
 		let attributes: [NSAttributedStringKey : Any] = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .headline)]
 		titleTextNode.attributedText = NSAttributedString(string: "Use Nodes in Node Containers", attributes: attributes)
 		titleTextNode.maximumNumberOfLines = 1
 
 		descriptionTextNode.attributedText =  NSAttributedString(string: "It is highly recommended that you use Texture’s nodes within a node container. Texture offers the following node containers.")
-
-		addSubnode(descriptionTextNode)
-		addSubnode(titleTextNode)
 	}
 
 	override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
